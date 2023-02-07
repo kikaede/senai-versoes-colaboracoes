e3dac43714ab8d3d73333f16e3a15979e5301ee7
Demetrius de Melo Souza Santos <123384496+kikaede@users.noreply.github.com>
2023-02-07 13:54:10 -0300

    Add files via upload

---
+++ jdoodle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+
+  int minhaIdade, maeIdade, paiIdade;
+    minhaIdade = 17;
+    maeIdade = 40;
+    paiIdade = 45;
+    
+    printf ("Minha idade é = %i\nIdade do Pai é = %i\nIdade do Mae é = %i.\n", minhaIdade, paiIdade, maeIdade);
+   
+}
