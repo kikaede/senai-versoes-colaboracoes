e604e13683fe6ed09eebc07c6a7756f06aac96a2
Demetrius de Melo Souza Santos <123384496+kikaede@users.noreply.github.com>
2023-02-07 14:51:54 -0300

    Add files via upload

---
+++ jdoodle (3).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int num1=5;
+    float num2=10.5;
+    float resultado;
+    
+    resultado = num1 / num2;
+    
+    printf("%f", resultado);
+    return 0;
+}
